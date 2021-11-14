70f8adbdaaf0cabb143299c0ae91b2da9ddf2e85
Obay M. Y. Almunirawi <omunirawi@gmail.com>
2021-11-14 21:34:02 +0200

    Initial Commit

---
+++ UDDER2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UDDER2
+//
+//  Created by Obay M. Y. Almunirawi on 14/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
